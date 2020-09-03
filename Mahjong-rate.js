833299eb8bcfbb4d969530883256c1145e5f1a43
Naru0903 <naru_0903@docomo.ne.jp>
2020-09-04 06:47:30 +0900

    3人用の追加、4人それぞれに順位ボーナスを付与

--- Mahjong-rate.js
+++ Mahjong-rate.js
@@ -3,9 +3,18 @@ const calculateButton = document.getElementById('calc');
 const resultDivided = document.getElementById('result-area');
 const tweetDivided = document.getElementById('tweet-area');
 const scoreTable = document.getElementById('score-table');
-const winnerBonus = 20; // 勝ち点
-const loserBonus = -10; // 負け点
-let rates = [0,0,0,0];
+let PLAYER_NUM = 0;
+if (window.location.href.split('/').pop() === 'Mahjong-rate.html') {
+    PLAYER_NUM = 4;
+}else if (window.location.href.split('/').pop() === 'Mahjong-rate3.html'){
+    PLAYER_NUM = 3;
+}
+const startPoint = 25000; // 持ち点
+const returnPoint = 30000; // 返し点
+// const rankBonus = Array(PLAYER_NUM);
+const rankBonus = [20, 10, -10, -20]; // 順位ボーナス
+const rates = Array(PLAYER_NUM);
+rates.fill(0);
 let count = 1;
 
 // 引数の要素の子要素をすべて削除する関数
@@ -14,9 +23,9 @@ function removeAllChildren(element) {
         element.removeChild(element.firstChild);
     }
 }
-// 昇順でソートするための関数
+// 降順でソートするための関数
 function compareFunc(a, b) {
-    return a - b;
+    return b - a;
 }
 
 // 計算ボタンを押したときの処理
@@ -24,17 +33,17 @@ calculateButton.onclick = () => {
 
     // プレイヤー名を受け取る
     const userNames = [];
-    for(let i = 1; i <= 4; i++) {
-        userNames.push(document.getElementById('user-name' + i).value);
+    for(let i = 1; i <= PLAYER_NUM; i++) {
         if (userNames[i-1] === '') { // プレイヤー名未入力で注意書きを出す
             alert('名前が未入力のプレイヤーがいます');
             return;
         }
+        userNames.push(document.getElementById('user-name' + i).value);
     }
 
     // 半荘の点数を受け取る(文字列を数字に変換している)
     const gameScores = [];
-    for(let i = 1; i <= 4; i++) {
+    for(let i = 1; i <= PLAYER_NUM; i++) {
         if (document.getElementById('score' + i + '-' + count).value === '') {
             alert('点数が未入力のプレイヤーがいます');
             return;
@@ -44,39 +53,38 @@ calculateButton.onclick = () => {
 
     // 点数の合計が合わないときにお知らせしてくれる
     let scoreSum = 0;
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < PLAYER_NUM; i++) {
         scoreSum += gameScores[i];
     }
-    if (scoreSum !== 100000) {
+    if (scoreSum !== startPoint*PLAYER_NUM) {
         alert('点数の合計が正しくありません');
         return;
     }
 
-
-
-    // 勝ち点、負け点の付与
-    const winnerScore = Math.max(gameScores[0],gameScores[1],gameScores[2],gameScores[3]);
-    const loserScore = Math.min(gameScores[0],gameScores[1],gameScores[2],gameScores[3]);
-    let bonusScores = [0,0,0,0];
-    for (let i = 0; i < 4; i++) {
-        if (gameScores[i] === winnerScore) {
-            bonusScores[i] += winnerBonus;
-        }
-        if (gameScores[i] === loserScore) {
-            bonusScores[i] += loserBonus;
+    // 順位ボーナスの設定、振り分けをする
+    const bonusScores = Array(PLAYER_NUM); // プレイヤーごとの勝ち点を格納する配列
+    const gameScoresCopy = Array.from(gameScores); // 半荘の点数の配列をコピー
+    gameScoresCopy.sort(compareFunc);
+    for (let i = 0; i < PLAYER_NUM; i++) {
+        for (let j = 0; j < PLAYER_NUM; j++) {
+            if ((bonusScores[j] === undefined) && (gameScoresCopy[i] === gameScores[j])) {
+                bonusScores[j] = rankBonus[i];
+            }
         }
     }
 
+
     // レートの合計を計算する
-    let gameRates = [0, 0, 0, 0];
-    for(let i = 1; i <= 4; i++) {
-        gameRates[i-1] = (gameScores[i-1] - 30000) * 0.001 + bonusScores[i-1];
-        rates[i-1] = rates[i-1] + gameRates[i-1];
+    const gameRates = Array(PLAYER_NUM);
+    gameRates.fill(0);
+    for(let i = 0; i < PLAYER_NUM; i++) {
+        gameRates[i] = (gameScores[i] - returnPoint) * 0.001 + bonusScores[i];
+        rates[i] = rates[i] + gameRates[i];
     }
 
     // レートを結果表示エリアに表示
     removeAllChildren(resultDivided);
-    for(let i = 0; i < 4; i++) {
+    for(let i = 0; i < PLAYER_NUM; i++) {
         const playerRate = document.createElement('p');
         playerRate.innerText = userNames[i] + '    ' + rates[i];
         resultDivided.appendChild(playerRate);
@@ -86,7 +94,7 @@ calculateButton.onclick = () => {
     // 表の行を新しく追加
     const newTableRow = scoreTable.insertRow(count+1);
     count++;
-    for(let i = 0; i < 4; i++) {
+    for(let i = 0; i < PLAYER_NUM; i++) {
         const newTableData = document.createElement('td');
         newTableRow.appendChild(newTableData);
         const newInput = document.createElement('input');
@@ -103,16 +111,19 @@ calculateButton.onclick = () => {
     // <a href="https://twitter.com/intent/tweet?button_hashtag=麻雀レート計算結果&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #今日の麻雀</a>
     // <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     removeAllChildren(tweetDivided);
-    const tweetText = userNames[0] + 'さんのレートは' + rates[0].toFixed(1) + '\n'
+    let tweetText = userNames[0] + 'さんのレートは' + rates[0].toFixed(1) + '\n'
                     + userNames[1] + 'さんのレートは' + rates[1].toFixed(1) + '\n'
                     + userNames[2] + 'さんのレートは' + rates[2].toFixed(1) + '\n'
-                    + userNames[3] + 'さんのレートは' + rates[3].toFixed(1) + ' でした。' + '\n';
+
+    if (PLAYER_NUM === 4) {
+        tweetText += userNames[3] + 'さんのレートは' + rates[3].toFixed(1) + ' でした。' + '\n';
+    }
     const anchor = document.createElement('a');
     const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag='
         + encodeURIComponent('今日の麻雀')
         + '&ref_src=twsrc%5Etfw';
     anchor.setAttribute('href', hrefValue);
-    //anchor.className = 'twitter-hashtag-button';
+    // ↓anchor.className = 'twitter-hashtag-button'と同一;
     anchor.setAttribute('class', 'twitter-hashtag-button');
     anchor.setAttribute('data-text', tweetText);
     anchor.setAttribute('data-show-count', 'false');
